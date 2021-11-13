369dfef81fa6a31a3cbd9938853294745a66ad4c
Kradon <kradonzhao@gmail.com>
2021-11-13 17:58:54 -0500

    dummy response for the weather api
    It returns a sample api response of what the expected output will look like from the weather api.

--- prototype/prototype/apis/views.py
+++ prototype/prototype/apis/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. This is a testing ground.")
+    json = [{"DateTime":"2021-11-13T18:00:00-05:00","EpochDateTime":1636844400,"WeatherIcon":15,"IconPhrase":"Thunderstorms","HasPrecipitation":True,"PrecipitationType":"Rain","PrecipitationIntensity":"Moderate","IsDaylight":False,"Temperature":{"Value":51.0,"Unit":"F","UnitType":18},"PrecipitationProbability":51,"MobileLink":"http://www.accuweather.com/en/us/boston-ma/02108/hourly-weather-forecast/348735?day=1&hbhhour=18&lang=en-us","Link":"http://www.accuweather.com/en/us/boston-ma/02108/hourly-weather-forecast/348735?day=1&hbhhour=18&lang=en-us"}]
+    return JsonResponse(json[0])
