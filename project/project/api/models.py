ae0a8e6cab696f7b112217c649343eecce58950a
Kradon <kradonzhao@gmail.com>
2021-12-12 22:46:03 -0500

    create zipcode models
    model will store the zipcode of the input as well as the location key of it.

--- project/project/api/models.py
+++ project/project/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ZipCodes(models.Model):
+    zip_code = models.CharField(max_length=10)
+    location_key = models.CharField(max_length=20)
