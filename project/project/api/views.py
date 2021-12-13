2d76d8198c39a60971a52041583e4126d1be0a24
Kradon <kradonzhao@gmail.com>
2021-12-13 16:07:20 -0500

    check database for existing zipcodes
    When the form is received, it checks to see if the zipcode and its location key is already stored.
    If it is, then it uses that rather than wasting a query on the api.

--- project/project/api/views.py
+++ project/project/api/views.py
@@ -11,23 +11,27 @@ weather = "BSVlWql7xKgdXKpqB7njSJDT3WHetovS"
 def forecast(request):
     base_url = "http://dataservice.accuweather.com/"
     fname = request.POST["fname"]
+
     try:
         if not zipcodes.is_real(fname):
             return JsonResponse({})
     except ValueError:
         return JsonResponse({})
+    if not ZipCode.objects.filter(zip_code=fname).exists():
+        response = requests.get(f"{base_url}/locations/v1/postalcodes/search?apikey={weather}&q={fname}")
+        if "Code" in response.json():
+            #error handle
+            return JsonResponse(response.json())
+        elif len(response.json()) == 0:
+            return JsonResponse({})
 
-    response = requests.get(f"{base_url}/locations/v1/postalcodes/search?apikey={weather}&q={fname}")
-    if "Code" in response.json():
-        #error handle
-        return JsonResponse(response.json())
-    elif len(response.json()) == 0:
-        return JsonResponse({})
+        key = response.json()[0]["Key"]
+        result_code = response.json()[0]["PrimaryPostalCode"]
 
-    key = response.json()[0]["Key"]
-    result_code = response.json()[0]["PrimaryPostalCode"]
-
-    entry = ZipCode.objects.create(zip_code = result_code, location_key = key)
+        entry = ZipCode.objects.create(zip_code = result_code, location_key = key)
+    else:
+        object = ZipCode.objects.get(zip_code = fname)
+        key = object.location_key
 
     response = requests.get(f"{base_url}/forecasts/v1/hourly/1hour/{key}?apikey={weather}&details=true")
     return JsonResponse(response.json()[0])
