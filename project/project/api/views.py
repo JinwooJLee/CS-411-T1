d26a352ef0d4371d7091f46d7944021003289a0b
Kradon <kradonzhao@gmail.com>
2021-12-13 15:56:52 -0500

    add forecast api call

--- project/project/api/views.py
+++ project/project/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from django.views.decorators.http import require_POST
+from .models import ZipCode
+import requests
+import zipcodes
 
-# Create your views here.
+weather = "BSVlWql7xKgdXKpqB7njSJDT3WHetovS"
+
+@require_POST
+def forecast(request):
+    base_url = "http://dataservice.accuweather.com/"
+    fname = request.POST["fname"]
+    try:
+        if not zipcodes.is_real(fname):
+            return JsonResponse({})
+    except ValueError:
+        return JsonResponse({})
+
+    response = requests.get(f"{base_url}/locations/v1/postalcodes/search?apikey={weather}&q={fname}")
+    if "Code" in response.json():
+        #error handle
+        return JsonResponse(response.json())
+    elif len(response.json()) == 0:
+        return JsonResponse({})
+
+    key = response.json()[0]["Key"]
+    result_code = response.json()[0]["PrimaryPostalCode"]
+
+    entry = ZipCode.objects.create(zip_code = result_code, location_key = key)
+
+    response = requests.get(f"{base_url}/forecasts/v1/hourly/1hour/{key}?apikey={weather}&details=true")
+    return JsonResponse(response.json()[0])
