f20d55eb1b6d2a8163ee98a3425a11f98daf0ad5
Kradon <kradonzhao@gmail.com>
2021-12-13 15:55:59 -0500

    add __str__ method for the model
    Model will print out the zipcode that it represents.
    Fixed name to be singular.

--- project/project/api/models.py
+++ project/project/api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class ZipCodes(models.Model):
+class ZipCode(models.Model):
     zip_code = models.CharField(max_length=10)
     location_key = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.zip_code
